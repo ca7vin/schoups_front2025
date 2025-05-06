a3a70861fdca12828c5b4aa2e5464a9ec359116b
ca7vin <ca7vin@gmail.com>
2025-05-06 09:49:14 +0200

    Modal Convervation + Titles fix

--- components/Products.vue
+++ components/Products.vue
@@ -1,7 +1,7 @@
 <template>
   <section id="products">
     <div class="bg-[#D4338B]">
-      <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-6xl text-center text-white py-6 md:py-8 xl:mb-5">
+      <h2 class="text-4xl sm:text-5xl md:text-6xl lg:text-6xl text-center text-white py-6 md:py-8">
         Nos produits
       </h2>
       <div class="flex justify-center mb-10">
@@ -34,40 +34,44 @@
             <button class="absolute top-4 right-4 text-white text-2xl"
               @click="showModalComment = false">&times;</button>
             <h2 class="text-5xl text-white text-center mb-6">Comment servir nos produits ?</h2>
+            <h2 class="text-3xl text-white text-center mb-6 underline">Température idéale : entre -16 et -18 degrés</h2>
             <div class="flex flex-col md:flex-row items-stretch gap-4">
-
               <!-- Bloc 1 -->
-              <div class="flex flex-col items-center justify-center w-full md:w-1/2 p-6 h-full flex-1">
-                <img src="../public/images/Step1-1-macaron.png" class="w-2/3" alt="">
+              <div class="flex flex-col items-start w-full md:w-1/3 p-6 flex-1">
+                <img src="../public/images/Step1-1-macaron.png" class="w-3/4 self-center" alt="">
                 <ul
-                  class="list-decimal flex flex-col items-center mt-4 space-y-2 max-w-sm text-center flex-1 justify-center">
-                  <li class="text-white text-xl">Tenez la cuillère au bout</li>
-                  <li class="text-white text-xl">Puisez par couche</li>
-                  <li class="text-white text-xl">Evitez que la glace se gèle aux parois du récipient</li>
+                  class="list-decimal list-inside mt-4 space-y-1.5 leading-snug max-w-sm text-left text-white text-base md:text-lg lg:text-xl">
+                  <li>Tenez la cuillère au bout</li>
+                  <li>Puisez par couche</li>
+                  <li>Évitez que la glace se gèle aux parois du récipient</li>
                 </ul>
               </div>
-
               <!-- Bloc 2 -->
-              <div class="flex flex-col items-center justify-center w-full md:w-1/3 p-6 h-full flex-1">
-                <img src="../public/images/Step2-1-macaron.png" class="w-2/3" alt="">
+              <div class="flex flex-col items-start w-full md:w-1/3 p-6 flex-1">
+                <img src="../public/images/Step2-1-macaron.png" class="w-3/4 self-center" alt="">
                 <ul
-                  class="list-decimal flex flex-col items-center mt-4 space-y-2 max-w-sm text-center flex-1 justify-center">
-                  <li class="text-white text-xl">Enlevez couche par couche jusqu'au fond du pot</li>
-                  <li class="text-white text-xl">La cuillère tranche chaque portion</li>
-                  <li class="text-white text-xl">Aucun risque d'abimer la matière</li>
+                  class="list-decimal list-inside mt-4 space-y-1.5 leading-snug max-w-sm text-left text-white text-base md:text-lg lg:text-xl">
+                  <li>Enlevez couche par couche jusqu'au fond du pot</li>
+                  <li>La cuillère tranche chaque portion</li>
+                  <li>Aucun risque d'abîmer la matière</li>
                 </ul>
               </div>
-
               <!-- Bloc 3 -->
-              <div class="flex flex-col items-center justify-center w-full md:w-1/3 p-6 h-full flex-1">
-                <img src="../public/images/Step3-1-macaron.png" class="w-2/3" alt="">
+              <div class="flex flex-col items-start w-full md:w-1/3 p-6 flex-1">
+                <img src="../public/images/Step3-1-macaron.png" class="w-3/4 self-center" alt="">
                 <ul
-                  class="list-decimal flex flex-col items-center mt-4 space-y-2 max-w-sm text-center flex-1 justify-center">
-                  <li class="text-white text-xl">La surface de la glace abaisse uniformément</li>
-                  <li class="text-white text-xl">Vous obtenez ainsi votre nombre de portions</li>
-                  <li class="text-white text-xl">Vous vous assurez de ne pas gaspiller en assurant la qualité</li>
+                  class="list-decimal list-inside mt-4 space-y-1.5 leading-snug max-w-sm text-left text-white text-base md:text-lg lg:text-xl">
+                  <li>La surface de la glace abaisse uniformément</li>
+                  <li>Vous obtenez ainsi votre nombre de portions</li>
+                  <li>Vous vous assurez de ne pas gaspiller en assurant la qualité</li>
                 </ul>
               </div>
+            </div>
+            <div class="flex items-center justify-center">
+              <button @click="showModalComment = false"
+                class="uppercase bg-white text-[#D4338B] px-6 py-4 rounded-xl font-semibold shadow-sm border border-white transition-all duration-300 ease-in-out hover:ring-2 hover:ring-[#D4338B] hover:bg-[#D4338B] hover:text-white hover:-translate-y-1 hover:shadow-lg">
+                Fermer
+              </button>
             </div>
           </div>
         </div>
