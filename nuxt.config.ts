b11528ea88f9cef7cf5a2134d63643f718577153
ca7vin <ca7vin@gmail.com>
2025-05-08 10:37:41 +0200

    favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,11 +10,25 @@ export default defineNuxtConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
   modules: ["@nuxt/fonts", "nuxt-swiper", "@vesp/nuxt-fontawesome"],
+
   fontawesome: {
     icons: {
       solid: ['chevron-left', 'chevron-right', 'bars'],
       brands: ['instagram', 'square-facebook'],
+    }
+  },
+
+  app: {
+    head: {
+      link: [
+        { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+        { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+        { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+        { rel: 'manifest', href: '/site.webmanifest' },
+        { rel: 'shortcut icon', href: '/favicon.ico' }
+      ]
     }
   }
 });
