37902d71d9cca0172798094c78fdc90697b076d0
ca7vin <ca7vin@gmail.com>
2025-05-09 23:15:23 +0200

    Swiper autoplay

--- components/Partners.vue
+++ components/Partners.vue
@@ -4,21 +4,29 @@
       <h2 class="uppercase text-4xl sm:text-5xl md:text-6xl lg:text-6xl py-6 md:py-8" style="color: #D4338B;">Nos partenaires</h2>
       <div class="w-full flex justify-between pb-10 items-center flex-wrap">
         <!-- Left Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper.prev()">
-          <font-awesome :style="{color: '#d3338b'}" icon="chevron-left" class="fa-4x" />
+        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper?.slidePrev()">
+          <font-awesome :style="{ color: '#d3338b' }" icon="chevron-left" class="fa-4x" />
         </button>
 
         <!-- Swiper Container -->
-        <swiper-container class="w-10/12 sm:w-10/12 md:w-10/12" ref="containerRef">
+        <swiper-container
+          class="w-10/12 sm:w-10/12 md:w-10/12"
+          ref="containerRef"
+          :autoplay="{ delay: 3000, disableOnInteraction: false }"
+          :loop="true"
+          @swiper="onSwiper"
+          @mouseenter="stopAutoplay"
+          @mouseleave="startAutoplay"
+        >
           <swiper-slide v-for="(partner, idx) in partners" :key="idx" style="background-color: #fff; color: #d3338b;">
             <img :src="partner.image" :alt="`image ${partner.nom}`"
-              class="object-cover w-2/3 sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/4 " />
+              class="object-cover w-2/3 sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/4" />
           </swiper-slide>
         </swiper-container>
 
         <!-- Right Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper.next()">
-          <font-awesome :style="{color: '#d3338b'}" icon="chevron-right" class="fa-4x" />
+        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper?.slideNext()">
+          <font-awesome :style="{ color: '#d3338b' }" icon="chevron-right" class="fa-4x" />
         </button>
       </div>
     </div>
@@ -28,18 +36,31 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
 import { useContent } from '~/composables/useContent'
+import type { Swiper } from 'swiper/types'
 
 const { content } = useContent()
 
-// Option 1 : directement en computed si la transformation est simple
 const partners = computed(() => {
   if (!Array.isArray(content.value?.partners)) return []
-
   return content.value.partners.map((partner: any) => ({
     nom: partner.nom,
     image: partner.image,
   }))
 })
+
+const swiper = ref<Swiper | null>(null)
+
+function onSwiper(instance: Swiper) {
+  swiper.value = instance
+}
+
+function stopAutoplay() {
+  swiper.value?.autoplay?.stop()
+}
+
+function startAutoplay() {
+  swiper.value?.autoplay?.start()
+}
 </script>
 
 <style scoped>
