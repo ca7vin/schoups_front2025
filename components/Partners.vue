346042afab765e041c1922286e798c7365eec7c3
Calvin Van der Ghinst <93983629+ca7vin@users.noreply.github.com>
2025-06-11 10:11:10 +0200

    Update Partners.vue

--- components/Partners.vue
+++ components/Partners.vue
@@ -4,7 +4,12 @@
       <h2 class="uppercase text-4xl sm:text-5xl md:text-6xl lg:text-6xl py-6 md:py-8" style="color: #D4338B;">Nos partenaires</h2>
       <div class="w-full flex justify-between pb-10 items-center flex-wrap">
         <!-- Left Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper?.slidePrev()">
+        <button
+          class="w-1/12 sm:w-1/6 md:w-1/12"
+          @click="slidePrev"
+          :disabled="!swiper"
+          type="button"
+        >
           <font-awesome :style="{ color: '#d3338b' }" icon="chevron-left" class="fa-4x" />
         </button>
 
@@ -18,14 +23,23 @@
           @mouseenter="stopAutoplay"
           @mouseleave="startAutoplay"
         >
-          <swiper-slide v-for="(partner, idx) in partners" :key="idx" style="background-color: #fff; color: #d3338b;">
+          <swiper-slide
+            v-for="(partner, idx) in partners"
+            :key="idx"
+            style="background-color: #fff; color: #d3338b;"
+          >
             <img :src="partner.image" :alt="`image ${partner.nom}`"
               class="object-cover w-2/3 sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/4" />
           </swiper-slide>
         </swiper-container>
 
         <!-- Right Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper?.slideNext()">
+        <button
+          class="w-1/12 sm:w-1/6 md:w-1/12"
+          @click="slideNext"
+          :disabled="!swiper"
+          type="button"
+        >
           <font-awesome :style="{ color: '#d3338b' }" icon="chevron-right" class="fa-4x" />
         </button>
       </div>
@@ -52,9 +66,18 @@ const swiper = ref<Swiper | null>(null)
 
 function onSwiper(instance: Swiper) {
   swiper.value = instance
+  // Optionnel : tu peux forcer autoplay ici si tu veux :
   setTimeout(() => {
     instance?.autoplay?.start?.()
   }, 50)
+}
+
+function slidePrev() {
+  swiper.value?.slidePrev()
+}
+
+function slideNext() {
+  swiper.value?.slideNext()
 }
 
 function stopAutoplay() {
@@ -64,49 +87,4 @@ function stopAutoplay() {
 function startAutoplay() {
   swiper.value?.autoplay?.start()
 }
-  onMounted(() => {
-  // Attend que tout soit prêt, puis essaie de démarrer autoplay
-  setTimeout(() => {
-    swiper.value?.autoplay?.start?.()
-  }, 300)
-})
 </script>
-
-<style scoped>
-swiper-slide {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 18px;
-  height: 40vh;
-  font-size: 4rem;
-  overflow: hidden;
-}
-
-/* Responsive design adjustments */
-@media screen and (max-width: 767px) {
-  .fa-4x {
-    display: none;
-  }
-
-  .swiper-slide img {
-    width: 70%;
-  }
-}
-
-@media screen and (min-width: 768px) and (max-width: 1024px) {
-  #partners {
-    padding: 30px;
-  }
-
-  .swiper-slide img {
-    width: 50%;
-  }
-}
-
-@media screen and (min-width: 1025px) {
-  .swiper-slide img {
-    width: 25%;
-  }
-}
-</style>
