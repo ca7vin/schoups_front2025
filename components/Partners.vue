30d496b7c960501af48e80feabd93e80507ae917
Calvin Van der Ghinst <93983629+ca7vin@users.noreply.github.com>
2025-06-11 10:23:26 +0200

    Update Partners.vue

--- components/Partners.vue
+++ components/Partners.vue
@@ -1,51 +1,30 @@
 <template>
   <section id="partners">
     <div class="w-full flex flex-col items-center">
-      <h2
-        class="uppercase text-4xl sm:text-5xl md:text-6xl lg:text-6xl py-6 md:py-8"
-        style="color: #D4338B;"
-      >
-        Nos partenaires
-      </h2>
+      <h2 class="uppercase text-4xl sm:text-5xl md:text-6xl lg:text-6xl py-6 md:py-8" style="color: #D4338B;">Nos partenaires</h2>
       <div class="w-full flex justify-between pb-10 items-center flex-wrap">
         <!-- Left Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="slidePrev">
-          <font-awesome
-            :style="{ color: '#d3338b' }"
-            icon="chevron-left"
-            class="fa-4x"
-          />
+        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper.prev()">
+          <font-awesome :style="{ color: '#d3338b' }" icon="chevron-left" class="fa-4x" />
         </button>
 
         <!-- Swiper Container -->
-        <Swiper
+        <swiper-container
           class="w-10/12 sm:w-10/12 md:w-10/12"
+          ref="containerRef"
           :autoplay="{ delay: 3000, disableOnInteraction: false }"
           :loop="true"
           @swiper="onSwiper"
-          @mouseenter="stopAutoplay"
-          @mouseleave="startAutoplay"
         >
-          <SwiperSlide
-            v-for="(partner, idx) in partners"
-            :key="idx"
-            style="background-color: #fff; color: #d3338b;"
-          >
-            <img
-              :src="partner.image"
-              :alt="`image ${partner.nom}`"
-              class="object-cover w-2/3 sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/4"
-            />
-          </SwiperSlide>
-        </Swiper>
+          <swiper-slide v-for="(partner, idx) in partners" :key="idx" style="background-color: #fff; color: #d3338b;">
+            <img :src="partner.image" :alt="`image ${partner.nom}`"
+              class="object-cover w-2/3 sm:w-1/3 md:w-1/2 lg:w-1/2 xl:w-1/4" />
+          </swiper-slide>
+        </swiper-container>
 
         <!-- Right Button -->
-        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="slideNext">
-          <font-awesome
-            :style="{ color: '#d3338b' }"
-            icon="chevron-right"
-            class="fa-4x"
-          />
+        <button class="w-1/12 sm:w-1/6 md:w-1/12" @click="swiper.prev()">
+          <font-awesome :style="{ color: '#d3338b' }" icon="chevron-right" class="fa-4x" />
         </button>
       </div>
     </div>
@@ -54,14 +33,8 @@
 
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import { Swiper, SwiperSlide } from 'swiper/vue'
-import SwiperCore, { Autoplay } from 'swiper'
 import { useContent } from '~/composables/useContent'
-import type { Swiper as SwiperType } from 'swiper'
-import 'swiper/css'
-import 'swiper/css/autoplay'
-
-SwiperCore.use([Autoplay])
+import type { Swiper } from 'swiper/types'
 
 const { content } = useContent()
 
@@ -73,29 +46,32 @@ const partners = computed(() => {
   }))
 })
 
-const swiper = ref<SwiperType | null>(null)
+const swiper = ref<Swiper | null>(null)
 
-function onSwiper(swiperInstance: SwiperType) {
-  swiper.value = swiperInstance
-}
-
-function slidePrev() {
-  swiper.value?.slidePrev()
-}
-function slideNext() {
-  swiper.value?.slideNext()
+function onSwiper(instance: Swiper) {
+  swiper.value = instance
+  setTimeout(() => {
+    instance?.autoplay?.start?.()
+  }, 50)
 }
 
 function stopAutoplay() {
   swiper.value?.autoplay?.stop()
 }
+
 function startAutoplay() {
   swiper.value?.autoplay?.start()
 }
+  onMounted(() => {
+  // Attend que tout soit prêt, puis essaie de démarrer autoplay
+  setTimeout(() => {
+    swiper.value?.autoplay?.start?.()
+  }, 300)
+})
 </script>
 
 <style scoped>
-.swiper-slide {
+swiper-slide {
   display: flex;
   justify-content: center;
   align-items: center;
